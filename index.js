0001cefe4daba39b4f789ae5d0b53a300a7c6d4a
Tanvirul islam <tanveer99ryan@gmail.com>
2025-05-02 02:21:07 +0600

    request data save and decrease No. of volunteers needed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2tkvzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -15,37 +15,78 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const volunteerCollection = client.db('volunteer-db').collection('volunteer')
+    const volunteerCollection = client
+      .db("volunteer-db")
+      .collection("volunteer");
+    const volunteerRequestCollection = client
+      .db("volunteer-db")
+      .collection("volunteer-request");
 
     // Volunteer add post
-app.post('/add-volunteer', async(req, res) =>{
-  const data = req.body
-  const result = await volunteerCollection.insertOne(data)
-  res.send(result)
-})
+    app.post("/add-volunteer", async (req, res) => {
+      const data = req.body;
+      const result = await volunteerCollection.insertOne(data);
+      res.send(result);
+    });
 
-// volunteer Data get with sort
-app.get('/get-volunteer', async(req, res) =>{
-  
+    // volunteer Data get with sort
+    app.get("/get-volunteer", async (req, res) => {
+      const result = await volunteerCollection
+        .find()
+        .sort({ date: 1 })
+        .toArray();
+      res.send(result);
+    });
 
+    // Volunteer get by id
+    app.get("/volunteer-get/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.findOne(query);
+      res.send(result);
+    });
 
-  const result = await volunteerCollection.find().sort({'date': 1}).toArray()
-  res.send(result)
-})
+    // Save volunteer request data in volunteerRequestCollection
+    app.post("/volunteer-request", async (req, res) => {
+      const volunteerRequestData = req.body;
 
+      // Same user do not request in same data
+      const query = {
+        organizer_email: volunteerRequestData.organizer_email,
+        id: volunteerRequestData.id,
+      };
+      const alreadyExist = await volunteerRequestCollection.findOne(query);
+      if (alreadyExist) {
+        return res.status(400).send("You have already requested in this");
+      }
+      // Save data
+      const result = await volunteerRequestCollection.insertOne(
+        volunteerRequestData
+      );
 
-
+      // Decrease No. of volunteers needed
+      const filter = { _id: new ObjectId(volunteerRequestData.id) };
+      const update = {
+        $inc: {
+          noOfVolunteersNeeded: -1,
+        },
+      };
+      const updated = await volunteerCollection.updateOne(filter, update)
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,11 +94,10 @@ app.get('/get-volunteer', async(req, res) =>{
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("The volunteer-management server ");
+});
 
-app.get('/', (req, res) =>{
-    res.send('The volunteer-management server ')
-})
-
-app.listen(port, () =>{
-    console.log('the volunteer management application running on port', port);
-})
+app.listen(port, () => {
+  console.log("the volunteer management application running on port", port);
+});
